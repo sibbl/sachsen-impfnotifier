8093dbb05d3daeca7290f4ed983e3acc459a2efc
Sebastian Schubotz <me@sibbl.net>
2021-05-02 17:46:35 +0200

    Add optional telegram notification

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ Node.js and npm are required. You also need to be registered on the vaccination 
    | PASSWORD | Yes | Your Password |
    | CENTRES | Yes | Comma separated list of centres to check: Belgern-Schildau, Borna, Kamenz, Chemnitz, Dresden, Treuen, Annaberg-Buchholz, Grimma, Löbau, Leipzig, Mittweida, Plauen, Pirna, Riesa, Zwickau |
    | DELAY | No | Delay between each check in ms (300000 ms = 5 min by default) |
-   | BROWSER_OPTIONS | No | Custom [browser options](https://playwright.dev/docs/api/class-browsertype#browsertypelaunchoptions) as JSON string (f.e. to configure a proxy)
+   | BROWSER_OPTIONS | No | Custom [browser options](https://playwright.dev/docs/api/class-browsertype#browsertypelaunchoptions) as JSON string (f.e. to configure a proxy) |
+   | TELEGRAM_TOKEN | No | Telegram bot token, if you want to be notified via Telegram |
+   | TELEGRAM_CHAT_ID | No | Telegram chat ID which you want to be notified at |
 
 3. Run `node index.js`
 4. Wait for the notification and be lucky
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 require('dotenv').config();
+process.env.NTBA_FIX_319 = 1; // disable telegram deprecated warnings on start up
 const { chromium } = require('playwright'),
-  notifier = require('node-notifier');
+  notifier = require('node-notifier'),
+  TelegramBot = require('node-telegram-bot-api');
 
 const MAX_32_BIT_SIGNED_INTEGER = Math.pow(2, 31) - 1;
-const browserOptions = { headless: false, ...(process.env.BROWSER_OPTIONS ? JSON.parse(process.env.BROWSER_OPTIONS) : {}) };
+const browserOptions = {
+  headless: false,
+  ...(process.env.BROWSER_OPTIONS
+    ? JSON.parse(process.env.BROWSER_OPTIONS)
+    : {}),
+};
+
+let notifyTelegramBot = null;
+if (process.env.TELEGRAM_TOKEN && process.env.TELEGRAM_CHAT_ID) {
+  notifyTelegramBot = (text) => {
+    const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+    return bot.sendMessage(process.env.TELEGRAM_CHAT_ID, text);
+  };
+  console.log('Telegram connection enabled');
+}
 
 (async () => {
   const centres = process.env.CENTRES.split(',').map((x) => x.trim());
@@ -45,10 +61,7 @@ const browserOptions = { headless: false, ...(process.env.BROWSER_OPTIONS ? JSON
       const noSuccessElement = await page.$('text=/keinen\\s*Termin/i');
       if (noSuccessElement === null) {
         console.log(`SUCCESS FOR ${currentCentre}`);
-        notifier.notify({
-          title: 'Impftermin gefunden!',
-          message: `Ein Termin für ${currentCentre} wurde gefunden!`,
-        });
+        await notifySuccess(currentCentre);
         return;
       }
       console.log(
@@ -66,4 +79,15 @@ async function waitFor(duration) {
   return new Promise((resolve) => {
     setTimeout(resolve, duration);
   });
+}
+
+async function notifySuccess(successCentre) {
+  const message = `Ein Termin für ${successCentre} wurde gefunden!`;
+  notifier.notify({
+    title: 'Impftermin gefunden!',
+    message,
+  });
+  if (notifyTelegramBot) {
+    await notifyTelegramBot(`${process.env.USER_NAME}: ${message}`);
+  }
 }
